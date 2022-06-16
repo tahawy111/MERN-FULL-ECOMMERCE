a2023e7b60a686463ab68e00f4c111875d023236
tahawy7 <amer.zp582@gmail.com>
2022-06-16 22:27:20 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      <header className="app-header">
+        <a href="/">Tahawy</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+};
+
+export default App;
