a0b5f5a6d5c3de472eb777924c90606c85417e50
tahawy7 <amer.zp582@gmail.com>
2022-06-16 23:20:03 +0200

    05- List Products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
+import data from './data';
+
 const App = () => {
   return (
     <div>
       <header className="app-header">
         <a href="/">Shopping</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+              <p>
+                <strong>{product.price}</strong>
+              </p>
+              <button>Add To Cart</button>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 };
