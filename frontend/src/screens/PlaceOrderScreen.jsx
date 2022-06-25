17c80dc86fc7496dc8cb585a9cae33f8ea87f7e0
tahawy7 <amer.zp582@gmail.com>
2022-06-25 22:24:19 +0200

    27- Create Place Order Screen

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -14,7 +14,15 @@ const PlaceOrderScreen = () => {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { cart, userInfo } = state;
 
-  const placeOrderHandler = (async) => {};
+  const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100; // 123.2345 => 123.23
+  cart.itemsPrice = round2(
+    cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
+  );
+  cart.shippingPrice = cart.itemsPrice > 100 ? round2(0) : round2(10);
+  cart.taxPrice = round2(0.15 * cart.itemsPrice);
+  cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+
+  const placeOrderHandler = async () => {};
   useEffect(() => {
     if (!cart.paymentMethod) {
       navigate('/payment');
